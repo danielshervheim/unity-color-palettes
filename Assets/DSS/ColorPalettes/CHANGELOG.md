27feca36bba4bc1af5df1f40bdb01b89313be2eb
Daniel Shervheim <danielshervheim@gmail.com>
2021-03-06 17:10:55 -0600

    added ApplyColorPaletteToButton script

--- Assets/DSS/ColorPalettes/CHANGELOG.md
+++ Assets/DSS/ColorPalettes/CHANGELOG.md
@@ -7,3 +7,7 @@
 ## 1.0.1
 
 - Refreshed GUIDs
+
+## 1.1.0
+
+- Added `ApplyColorPaletteToButton` class for handling the special case of buttons (which often change color during interaction).
---
+++ Assets/DSS/ColorPalettes/Runtime/Scripts/ApplyColorPaletteToButton.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DSS.ColorPalettes
+{
+    // @brief A special color palette applicator, specifically meant for Buttons.
+    // It allows you to specify two different color palette entries for the Button
+    // (and any contained graphics), that change depending on wether the button is
+    // clicked or not.
+    [RequireComponent(typeof(Button))]
+    [ExecuteInEditMode]
+    public class ApplyColorPaletteToButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [System.Serializable]
+        public class ApplicationTarget
+        {
+            public Graphic graphic;
+            public string clickedEntryName;
+            public string unclickedEntryName;
+        }
+
+        [SerializeField] ColorPalette preset = default;
+        [SerializeField] ApplicationTarget[] targets;
+
+        [SerializeField] AnimationCurve lerpCurve = AnimationCurve.Linear(0f,0f,1f,1f);
+        [SerializeField] float lerpDuration = 0.25f;
+
+        float timer = 0f;
+        bool clicked = false;
+
+
+        void Update()
+        {
+            if (preset == null)
+            {
+                return;
+            }
+
+            timer += (clicked ? 1f : -1f) * Time.deltaTime / lerpDuration;
+            timer = Mathf.Clamp01(timer);
+
+            foreach (ApplicationTarget target in targets)
+            {
+                if (target == null || target.graphic == null)
+                {
+                    continue;
+                }
+                target.graphic.color = Color.Lerp(preset.GetColor(target.unclickedEntryName), preset.GetColor(target.clickedEntryName), lerpCurve.Evaluate(timer));
+            }
+        }
+
+        public void OnPointerDown(PointerEventData data)
+        {
+            clicked = true;
+        }
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            clicked = false;
+        }
+    }
+}
